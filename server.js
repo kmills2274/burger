ec3a97036b98c7724c399d40425d7f206e4738cc
kate.m <katiemills0810@gmail.com>
2020-09-27 13:45:01 -0500

    fixed error with port setting

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
+var express = require("express");
 
+var PORT = process.env.PORT || 8000;
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-  var routes = require("./controllers/burgers_controller.js");
-  app.use("/", routes);
+var routes = require("./controllers/burgersController.js");
 
-  var PORT = 3000;
-  app.listen(PORT, function() {
-    console.log("Listening on PORT " + PORT);
-  });
-  
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
+});
