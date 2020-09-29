4ef7bf3bc725ce7ca2bd8c44391299c33e8b3504
kate.m <katiemills0810@gmail.com>
2020-09-28 22:32:34 -0500

    added jawsbd addon

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
 // MySQL connection
 var mysql = require("mysql");
 
- connection= mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'password',
-    database: "burgers_db"
+ var connection;
+
+ if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {  
+  connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "burgers_db"
 });
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("Error connecting: " + err.stack);
-    return;
-  }
-  console.log("Connected as id: " + connection.threadId);
-});
+};
+
+connection.connect();
 
 // Export connection 
 module.exports = connection;
